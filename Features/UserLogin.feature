ddfb7c1baa848384aa079303de0c334a1b844f67
priyanka <priyankatripathy81@gmail.com>
2024-02-19 20:48:14 +0530

    Adding files

---
+++ Features/UserLogin.feature
@@ -0,0 +1,15 @@
+﻿Feature: UserLogin
+
+A short summary of the feature
+
+@mytag
+Scenario:Successfull UserLogin
+Given a user with valid username "sam1256" and valid password "sam@1256@1256"
+
+When the Login method is called
+Then the result ahould Login Success
+
+Scenario: UnSuccessfull UserLogin
+Given a user with invalid username "Invalid_user" and invalid password "invalid"
+When the Login method is called
+Then  the result should Login Failed
---
+++ StepDefinitions/LoginStepDefinitions.cs
@@ -0,0 +1,63 @@
+﻿using UserLoginLib;
+namespace SpecFlowProjectUSLogin.StepDefinitions
+{
+    [Binding]
+    public  class LoginStepDefinitions
+    {
+        private string userName;
+
+        private string password;
+
+        private string loginResult;
+
+        private readonly ScenarioContext scenarioContext;
+
+        private readonly UserLogin userLogin;
+        public LoginStepDefinitions(ScenarioContext sc)
+        {
+
+            this.scenarioContext = sc;
+
+            userLogin = new UserLoginLib.UserLogin();
+        }
+        [Given(@"a user with valid username ""([^""]*)"" and valid password ""([^""]*)""")]
+
+        public void GivenAUserWithValidUsernameAndValidPassword(string p0, string p1)
+        {
+
+            userName = p0;
+
+            password = p1;
+        }
+
+        [When(@"the Login method is called")]
+        public void WhenTheLoginMethodIsCalled()
+
+          {
+
+        loginResult = userLogin.Login(userName, password);
+
+           }
+
+           [Then(@"the result should Login Success")]
+          public void WhenTheResultShouldLoginSuccess()
+        {
+            Assert.Equal("Login Success", loginResult);
+        }
+        [Given(@"a user with invalid username ""([^""]*)"" and invalid password ""([^""]*)""" )]
+         public void GivenAUserWithInvalidUsernameAndInvalidPassword(string p0, string invalid)
+        {
+            userName = p0;
+            userName =invalid;
+        }
+
+        [Then(@"the result should Login Failed")]
+
+        public void ThenThelResultShouldLoginFailed()
+        {
+            Assert.Equal("Login Failed", loginResult);
+        }
+    }
+
+
+}
